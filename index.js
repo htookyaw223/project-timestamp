1c7fc7c3aa31016eae1a27d7bb8b77e4c915deba
Htoo Kyaw <htookyaw326@gmail.com>
2024-11-17 13:17:36 +0700

    add api whoami

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  });
+});
 app.get("/api/:date?", function (req, res) {
   try {
     const input = req.params.date; // Optional date parameter
@@ -56,13 +63,6 @@ app.get("/api/:date?", function (req, res) {
   }
 });
 
-app.get("/api/whoami", function (req, res) {
-  res.json({
-    ipaddress: req.ip,
-    language: req.headers["accept-language"],
-    software: req.headers["user-agent"],
-  });
-});
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
